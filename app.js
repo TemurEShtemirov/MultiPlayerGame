3679f2dbcb74c120d7f5069ebee1c12f17c2c4ff
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-18 22:24:53 +0500

    socket.io setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import http from 'http'
+import { Server } from 'socket.io'
 
 async function bootstrap() {
   const app = express()
   const server = http.createServer(app)
+  // socket.io setup
+  const io = new Server(server)
+  
   const port = 3000
 
   app.use(express.static('public'))
@@ -12,7 +16,7 @@ async function bootstrap() {
     res.sendFile(__dirname + '/index.html')
   })
 
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`App listening on port ${port}`)
   })
 }
