a17ee61c4550972183b72eaafbcc020c778dcee1
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-29 15:11:37 +0500

    remove players on disconnect and timeout

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = express()
   const server = http.createServer(app)
   // socket.io setup
-  const io = new Server(server)
-  
+  const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
   const port = 3000
 
   app.use(express.static('public'))
@@ -16,8 +16,33 @@ async function bootstrap() {
     res.sendFile(__dirname + '/index.html')
   })
 
-  io.on('connection',(socket)=>{
-    console.log('A USER IS CONNECTED');
+  const players = {}
+  // const colors = ['yellow', 'green', 'orange', 'red', 'purple', 'pink']
+
+  // function randomXToY(minVal, maxVal) {
+  //   var randVal = minVal + Math.random() * (maxVal - minVal)
+  //   return Math.round(randVal)
+  // }
+  io.on('connection', (socket) => {
+    console.log('A USER IS CONNECTED')
+
+    players[socket.id] = {
+      x: 500 * Math.random(),
+      y: 500 * Math.random()
+      // x: randomXToY(101, 999),
+      // y: randomXToY(101, 999)
+      // color: colors[Math.floor(Math.random() * colors.length)]
+    }
+
+    io.emit('updatePlayers', players)
+
+    socket.on('disconnect', (reason) => {
+      console.log(reason)
+      delete players[socket.id]
+      io.emit('updatePlayers', players)
+    })
+
+    console.log(players)
   })
 
   server.listen(port, () => {
