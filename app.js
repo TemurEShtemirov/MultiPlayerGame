3b0d4a418b67c083defb4c2c294d02f7f34f02fc
TemurEShtemirov <temureshtemirov10@gmail.com>
2024-02-29 17:52:50 +0500

    almost finished

--- app.js
+++ app.js
@@ -1,53 +1,204 @@
+// import express from 'express'
+// import http from 'http'
+// import { Server } from 'socket.io'
+
+// async function bootstrap() {
+//   const app = express()
+//   const server = http.createServer(app)
+//   // socket.io setup
+//   const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
+//   const port = 3000
+
+//   app.use(express.static('public'))
+
+//   app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+//   })
+
+//   const backEndPlayers = {}
+
+//   io.on('connection', (socket) => {
+//     console.log('A USER IS CONNECTED')
+
+//     backEndPlayers[socket.id] = {
+//       x: 500 * Math.random(),
+//       y: 500 * Math.random(),
+//       color: `hsl(${360 * Math.random()}, 100%, 50%)`
+//     }
+
+//     io.emit('updatePlayers', backEndPlayers)
+
+//     socket.on('disconnect', (reason) => {
+//       console.log(reason)
+//       delete backEndPlayers[socket.id]
+//       io.emit('updatePlayers', backEndPlayers)
+//     })
+
+//     console.log(backEndPlayers)
+//   })
+
+//   server.listen(port, () => {
+//     console.log(`App listening on port ${port}`)
+//   })
+// }
+
+// bootstrap()
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
 
-async function bootstrap() {
-  const app = express()
-  const server = http.createServer(app)
-  // socket.io setup
-  const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
-  const port = 3000
+const port = 3000
 
-  app.use(express.static('public'))
+app.use(express.static('public'))
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
 
-  const players = {}
-  // const colors = ['yellow', 'green', 'orange', 'red', 'purple', 'pink']
+const backEndPlayers = {}
+const backEndProjectiles = {}
 
-  // function randomXToY(minVal, maxVal) {
-  //   var randVal = minVal + Math.random() * (maxVal - minVal)
-  //   return Math.round(randVal)
-  // }
-  io.on('connection', (socket) => {
-    console.log('A USER IS CONNECTED')
+const SPEED = 5
+const RADIUS = 10
+const PROJECTILE_RADIUS = 5
+let projectileId = 0
 
-    players[socket.id] = {
-      x: 500 * Math.random(),
-      y: 500 * Math.random()
-      // x: randomXToY(101, 999),
-      // y: randomXToY(101, 999)
-      // color: colors[Math.floor(Math.random() * colors.length)]
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  io.emit('updatePlayers', backEndPlayers)
+
+  socket.on('shoot', ({ x, y, angle }) => {
+    projectileId++
+
+    const velocity = {
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5
     }
 
-    io.emit('updatePlayers', players)
+    backEndProjectiles[projectileId] = {
+      x,
+      y,
+      velocity,
+      playerId: socket.id
+    }
 
-    socket.on('disconnect', (reason) => {
-      console.log(reason)
-      delete players[socket.id]
-      io.emit('updatePlayers', players)
-    })
-
-    console.log(players)
+    console.log(backEndProjectiles)
   })
 
-  server.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+  socket.on('initGame', ({ username, width, height }) => {
+    backEndPlayers[socket.id] = {
+      x: 1024 * Math.random(),
+      y: 576 * Math.random(),
+      color: `hsl(${Math.floor(360 * Math.random())}, 100%, 50%)`,
+      sequenceNumber: 0,
+      score: 0,
+      username,
+      canvas: {
+        width,
+        height
+      },
+      radius: RADIUS
+    }
   })
-}
 
-bootstrap()
+  socket.on('disconnect', (reason) => {
+    console.log(reason)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
+  })
+
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    const backEndPlayer = backEndPlayers[socket.id]
+
+    if (!backEndPlayers[socket.id]) return
+
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
+    switch (keycode) {
+      case 'KeyW':
+        backEndPlayers[socket.id].y -= SPEED
+        break
+      case 'KeyA':
+        backEndPlayers[socket.id].x -= SPEED
+        break
+      case 'KeyS':
+        backEndPlayers[socket.id].y += SPEED
+        break
+      case 'KeyD':
+        backEndPlayers[socket.id].x += SPEED
+        break
+    }
+
+    const playerSides = {
+      left: backEndPlayer.x - backEndPlayer.radius,
+      right: backEndPlayer.x + backEndPlayer.radius,
+      top: backEndPlayer.y - backEndPlayer.radius,
+      bottom: backEndPlayer.y + backEndPlayer.radius
+    }
+
+    if (playerSides.left < 0) backEndPlayers[socket.id].x = backEndPlayer.radius
+
+    if (playerSides.right > 1024)
+      backEndPlayers[socket.id].x = 1024 - backEndPlayer.radius
+
+    if (playerSides.top < 0) backEndPlayers[socket.id].y = backEndPlayer.radius
+
+    if (playerSides.bottom > 576)
+      backEndPlayers[socket.id].y = 576 - backEndPlayer.radius
+  })
+})
+
+// Backend ticker
+setInterval(() => {
+  for (const id in backEndProjectiles) {
+    backEndProjectiles[id].x += backEndProjectiles[id].velocity.x
+    backEndProjectiles[id].y += backEndProjectiles[id].velocity.y
+
+    if (
+      backEndProjectiles[id].x - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.width ||
+      backEndProjectiles[id].x + PROJECTILE_RADIUS <= 0 ||
+      backEndProjectiles[id].y - PROJECTILE_RADIUS >=
+        backEndPlayers[backEndProjectiles[id].playerId]?.canvas?.height ||
+      backEndProjectiles[id].y + PROJECTILE_RADIUS <= 0
+    ) {
+      delete backEndProjectiles[id]
+      continue
+    }
+
+    for (const playerId in backEndPlayers) {
+      const backEndPlayer = backEndPlayers[playerId]
+
+      const DISTANCE = Math.hypot(
+        backEndProjectiles[id].x - backEndPlayer.x,
+        backEndProjectiles[id].y - backEndPlayer.y
+      )
+
+      if (
+        DISTANCE < PROJECTILE_RADIUS + backEndPlayer.radius &&
+        backEndProjectiles[id].playerId !== playerId
+      ) {
+        if (backEndPlayers[backEndProjectiles[id].playerId])
+          backEndPlayers[backEndProjectiles[id].playerId].score++
+
+        delete backEndProjectiles[id]
+        delete backEndPlayers[playerId]
+        break
+      }
+    }
+  }
+
+  io.emit('updateProjectiles', backEndProjectiles)
+  io.emit('updatePlayers', backEndPlayers)
+}, 15)
+
+server.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
+console.log('server did load')
